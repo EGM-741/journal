911e106095c6fb87cc7e31ede0906cbeb7db571b
EGM-741 <132098068+EGM-741@users.noreply.github.com>
2024-07-31 09:24:01 +0300

    Add files via upload

--- journal.js
+++ journal.js
@@ -6,53 +6,25 @@ class Journal extends HTMLElement {
     render() {
         const STYLE = `
             <style>
-                .selectors-panel {
+                .table {
+                    margin-top: 40px;
+                    padding-left: 0px;
+                }
+                .title {
                     padding: 0px 20px 0px 20px;
                 }
-                .selectors {
-                    display: grid;
-                    grid-template-columns: 1fr 1fr 1fr;
-                    grid-auto-flow: row;
-                    width: 100%;
-                    max-width: 1700px;
-                    height: min-content;
-                }
-                .selector {
-                    text-align: center;
-                    align-items: center;
-                    justify-content: center;
-                    position: relative;
-                    display: flex;
-                    height: 48px;
-                    border: 1px;
-                    border-style: solid;
-                    border-color:rgb(211, 211, 211);
-                    background-color: rgba(255,0,0,0);
-                    color: rgb(128, 128, 128);
-                    cursor: pointer;
-                }
-                    .selector#delete:hover {
-                        background-color: rgb(255,0,0);
-                        color: rgb(255,255,255);
-                    }
-                .selector-text {
+                .table-title {
+                    padding: 0px;
+                    margin: 0px;
                     font-family: 'Inter-Medium';
-                    font-size: 18px;
-                    text-transform: uppercase;
-                }
-                .selector-icon {
-                    margin-right: 10px;
-                }
-                .span.add_new {
-                    color: rgb(128, 128, 128);
                 }
             </style>
         `;
         this.innerHTML = `
             ${STYLE}
-            <div class="selectors-panel">
-            <div class="title-wrapper table-title medium">
-                <h3>Журнал учебных заведений</h3>
+            <div class="table">
+            <div class="title">
+                <h3 class="table-title">Журнал учебных заведений</h3>
             </div>
             </div>
         `
